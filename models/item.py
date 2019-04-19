eb40714d28f51ac8a3ca4ba6df7651ebe0480bac
Vipul <vipul@Macintosh.local>
2019-04-20 02:42:31 +0530

    first commit

---
+++ models/item.py
@@ -0,0 +1,24 @@
+from db import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+ 
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, store_id):
+        self.name = name
+        
+        self.store_id = store_id
+
+    def json(self):
+        return {'name': self.name, }
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
